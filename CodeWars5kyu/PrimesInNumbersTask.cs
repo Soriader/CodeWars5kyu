2accadce5857797931e59484d12ff57ab04d0191
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-30 17:05:25 +0200

    [~] Task: Primes in numbers

---
+++ CodeWars5kyu/PrimesInNumbersTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars5kyu
+{
+    public class PrimesInNumbersTask
+    {
+        public static String Factors(int lst)
+        {
+            string result = "";
+            int count = 0;
+            for (int i = 2; i <= lst; i++)
+            {
+                count = 0;
+                while (lst % i == 0)
+                {
+                    count++;
+                    lst /= i;
+                }
+                if (count > 0)
+                {
+                    result += "(" + i;
+                    if (count > 1)
+                    {
+                        result += "**" + count;
+                    }
+                    result += ")";
+                }
+            }
+
+            return result;
+        }
+    }
+}
+//https://chat.openai.com/c/28395c0e-7528-4740-a026-ec220c04082e
--- UdemyTest/UnitTest1.cs
+++ UdemyTest/UnitTest1.cs
@@ -10,6 +10,12 @@ namespace UdemyTest
             class Test
             {
                 [Test]
+                public void PrimesInNumbersTest()
+                {
+                    Assert.AreEqual("(2**2)(3**3)(5)(7)(11**2)(17)", PrimesInNumbersTask.Factors(7775460));
+                }
+
+                [Test]
                 public void TakeTheDerivativeTest()
                 {
                     Assert.AreEqual("grfg", Rot13Task.Rot13("test"), String.Format("Input: test, Expected Output: grfg, Actual Output: {0}", Rot13Task.Rot13("test")));
