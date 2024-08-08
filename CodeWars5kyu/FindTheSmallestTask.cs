e6186cfe7f170ed7c9b7f269c361c7efded470cd
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-08-08 20:08:06 +0200

    [~] Task: Find the smallest

---
+++ CodeWars5kyu/FindTheSmallestTask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars5kyu
+{
+	public class FindTheSmallestTask
+	{
+		public static long[] Smallest(long n)
+		{
+			string numStr = n.ToString();
+			int len = numStr.Length;
+			long minValue = n;
+			int fromIndex = 0, toIndex = 0;
+
+			for (int i = 0; i < len; i++)
+			{
+				for (int j = 0; j < len; j++)
+				{
+					if (i == j) continue;
+
+					string modifiedStr = MoveDigit(numStr, i, j);
+					long modifiedValue = long.Parse(modifiedStr);
+
+					if (modifiedValue < minValue)
+					{
+						minValue = modifiedValue;
+						fromIndex = i;
+						toIndex = j;
+					}
+				}
+			}
+
+			return new long[] { minValue, fromIndex, toIndex };
+		}
+
+		private static string MoveDigit(string numStr, int fromIndex, int toIndex)
+		{
+			char digit = numStr[fromIndex];
+			string result = numStr.Remove(fromIndex, 1).Insert(toIndex, digit.ToString());
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/573992c724fc289553000e95/train/csharp
--- UdemyTest/UnitTest1.cs
+++ UdemyTest/UnitTest1.cs
@@ -10,6 +10,25 @@ namespace UdemyTest
             class Test
             {
 				[Test]
+				public static void FindTheSmallestTest()
+				{
+					testing(261235, "[126235, 2, 0]");
+					testing(209917, "[29917, 0, 1]");
+					testing(285365, "[238565, 3, 1]");
+					testing(269045, "[26945, 3, 0]");
+					testing(296837, "[239687, 4, 1]");
+
+					static void testing(long n, string res)
+					{
+						Assert.AreEqual(res, Array2String(FindTheSmallestTask.Smallest(n)));
+					}
+					static string Array2String(long[] list)
+					{
+						return "[" + string.Join(", ", list) + "]";
+					}
+				}
+
+				[Test]
 				public void WhatIsAPerfectPowerAnywayTest()
 				{
 					Assert.IsNull(WhatIsAPerfectPowerAnywayTask.IsPerfectPower(5), "5 is not a perfect power");
