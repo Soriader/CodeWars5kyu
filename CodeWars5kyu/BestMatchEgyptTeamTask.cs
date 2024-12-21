941ec108e43ed62beab4bb893a796dc987629afc
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-21 20:40:04 +0100

    [~] Task: Simple Fun #166: Best Match

---
+++ CodeWars5kyu/BestMatchEgyptTeamTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars5kyu
+{
+    public class BestMatchEgyptTeamTask
+    {
+        public int BestMatch(int[] goals1, int[] goals2)
+        {
+            int bestMatchIndex = -1;
+            int smallestDifference = int.MaxValue;
+            int mostGoals = -1;
+
+            for (int i = 0; i < goals1.Length; i++)
+            {
+                int difference = goals1[i] - goals2[i];
+
+                if (difference < smallestDifference)
+                {
+                    smallestDifference = difference;
+                    mostGoals = goals2[i]; 
+                    bestMatchIndex = i;
+                }
+                else if (difference == smallestDifference && goals2[i] > mostGoals)
+                {
+                    mostGoals = goals2[i];
+                    bestMatchIndex = i;
+                }
+            }
+
+            return bestMatchIndex;
+        }
+    }
+}
+//https://www.codewars.com/kata/58b38256e51f1c2af0000081/train/csharp
--- UdemyTest/UnitTest1.cs
+++ UdemyTest/UnitTest1.cs
@@ -10,7 +10,24 @@ namespace UdemyTest
         {
             class Test
             {
-				[Test]
+                [Test]
+                public void BestMatchEgyptTeamTest()
+                {
+                    var kata = new BestMatchEgyptTeamTask();
+
+                    //Assert.AreEqual(1, kata.BestMatch(new int[] { 6, 4 }, new int[] { 1, 2 }));
+
+                    Assert.AreEqual(0, kata.BestMatch(new int[] { 1 }, new int[] { 0 }));
+
+                    Assert.AreEqual(4, kata.BestMatch(new int[] { 1, 2, 3, 4, 5 }, new int[] { 0, 1, 2, 3, 4 }));
+
+                    Assert.AreEqual(2, kata.BestMatch(new int[] { 3, 4, 3 }, new int[] { 1, 1, 2 }));
+
+                    Assert.AreEqual(1, kata.BestMatch(new int[] { 4, 3, 4 }, new int[] { 1, 1, 1 }));
+
+                }
+
+                [Test]
 				public void PokerCardsTest()
 				{
 					Assert.AreEqual(new[] { 7, 22, 51 }, PokerCardsTask.Encode(new[] { "Td", "8c", "Ks" }));
