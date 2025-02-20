ae64bc0ba5dbf50123bfc0916f9c026081d5b6f4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-20 20:48:49 +0100

    [~] Task: Hamster me

---
+++ CodeWars5kyu/HamsterMeTask.cs
@@ -0,0 +1,32 @@
+﻿namespace CodeWars5kyu;
+
+public class HamsterMeTask
+{
+    public static string HamsterMe(string code, string message) 
+    {
+        var key = new Dictionary<char, string>();
+        code = string.Concat(code.Distinct().OrderBy(c => c));
+        
+        for (int i = 0; i < code.Length; i++)
+        {
+            var letter = code[i];
+            var nextLetter = i + 1 < code.Length ? code[i + 1] : (char)(code[0] + 26);
+            
+            for (int j = letter, k = 1; j < nextLetter; j++, k++)
+            {
+                if (j <= 'z')
+                {
+                    key[(char)j] = $"{letter}{k}";
+                }
+                else
+                {
+                    key[(char)(j - 26)] = $"{letter}{k}";
+                }
+            }
+        }
+        return string.Join("", message.Select(c => $"{key[c]}"));
+    }
+    
+    
+}
+//https://www.codewars.com/kata/595ddfe2fc339d8a7d000089/train/csharp
---
+++ UdemyTest/HamsterMe.cs
@@ -0,0 +1,18 @@
+﻿namespace UdemyTest;
+
+using CodeWars5kyu;
+
+public class HamsterMe
+{
+    [Test]
+    public void HamsterMeTest()
+    {
+        Assert.That(HamsterMeTask.HamsterMe("hamster", "hamster"), Is.EqualTo("h1a1m1s1t1e1r1"));
+        Assert.That(HamsterMeTask.HamsterMe("hamster", "helpme"), Is.EqualTo("h1e1h5m4m1e1"));
+        Assert.That(HamsterMeTask.HamsterMe("hmster", "hamster"), Is.EqualTo("h1t8m1s1t1e1r1"));   
+        Assert.That(HamsterMeTask.HamsterMe("hhhhammmstteree", "hamster"), Is.EqualTo("h1a1m1s1t1e1r1"));  
+        Assert.That(HamsterMeTask.HamsterMe("hamster", "abcdefghijklmnopqrstuvwxyz"), Is.EqualTo("a1a2a3a4e1e2e3h1h2h3h4h5m1m2m3m4m5r1s1t1t2t3t4t5t6t7"));  
+        Assert.That(HamsterMeTask.HamsterMe("f", "abcdefghijklmnopqrstuvwxyz"), Is.EqualTo("f22f23f24f25f26f1f2f3f4f5f6f7f8f9f10f11f12f13f14f15f16f17f18f19f20f21"));        
+    }
+}
+
