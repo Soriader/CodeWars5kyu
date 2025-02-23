86501ed04b9ea6e98dbe7ca8932a534038f52a88
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-23 18:22:00 +0100

    [~] Task: Coding with Squared Strings

---
+++ CodeWars5kyu/CodingWithSquaredStringsTask.cs
@@ -0,0 +1,54 @@
+﻿namespace CodeWars5kyu;
+
+public class CodingWithSquaredStringsTask
+{
+    public static string Code(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return s;
+        
+        int l = s.Length;
+        int n = (int)Math.Ceiling(Math.Sqrt(l));
+        char paddingChar = (char)11;
+
+        s = s.PadRight(n * n, paddingChar);
+        
+        char[,] grid = new char[n, n];
+        for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            grid[i, j] = s[i * n + j];
+
+        char[,] rotated = new char[n, n];
+        for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            rotated[j, n - 1 - i] = grid[i, j];
+
+        return string.Join("\n", Enumerable.Range(0, n)
+            .Select(row => new string(Enumerable.Range(0, n)
+                .Select(col => rotated[row, col]).ToArray())));
+    }
+
+    public static string Decode(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return s;
+        
+        string[] lines = s.Split('\n');
+        int n = lines.Length;
+        char[,] grid = new char[n, n];
+
+        for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            grid[i, j] = lines[i][j];
+
+        char[,] rotated = new char[n, n];
+        for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            rotated[n - 1 - j, i] = grid[i, j];
+
+        string result = string.Join("", Enumerable.Range(0, n)
+            .SelectMany(row => Enumerable.Range(0, n)
+                .Select(col => rotated[row, col])));
+
+        return result.TrimEnd((char)11);
+    }
+}
+//https://www.codewars.com/kata/56fcc393c5957c666900024d/train/csharp
---
+++ UdemyTest/CodingWithSquaredStrings.cs
@@ -0,0 +1,40 @@
+﻿namespace UdemyTest;
+
+using CodeWars5kyu;
+
+public class CodingWithSquaredStrings
+{
+    static string data1 = "What do you remember? When I looked at his streaky glasses, I wanted " +
+                          "to leave him. And before that? He stole those cherries for me at midnight. We were walking " +
+                          "in the rain and I loved him. And before that? I saw him coming " +
+                          "toward me that time at the picnic, edgy, foreign.";
+    static string data2 = "Some say the world will end in fire, Some say in ice. From what I've tasted of desire " +
+                          "I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate" +
+                          " To say that for destruction ice Is also great And would suffice.";
+    
+    static string data1Sol =
+        "\vctg?.nadr d gdbW\n\v,i    lnis tl eh\n\v mtIAakietboaara\n\veeo nnigsoe st?t\n\vd wsddnh lfls   \n\vgaaa  gtfeoeehWd\n" +
+        "\vytrwbI .o rasiho\n\v, d e i rtev,se \n\v t hflnW h e  ny\n\vfhmioo emot Is o\n\voeemrvt eshh tIu\n\vr   eehw eaiwr  \n" +
+        "\veptc deea tmaelr\n\viihot  rtc?.naoe\n\vgcamhhre h  tkom\n\vnntiaia meHAeyke\n\v.i ntmiwirend em";
+
+    static string data2Sol =
+        "fa  h ttrheI ilS\nitifakw   s'irdo\nc cotnihftivce m\neAereocaihree,we\n.n   wedroe . i \n\vdIdT , es t Sls\n\v seoe t.eIaFola\n" +
+        "\vw s nIo   srm y\n\voatso  Bwhtoee \n\vulrautpuhoem nt\n\vlsuyghetold sdh\n\vdoc hir  d wa e\n\v  tt niif ohyi \n\vsgihoksfawfa nw\n" +
+        "\vuroaf h vi ti o\n\vfent I iotd nfr";
+
+    private static void testing(string actual, string expected) 
+    {
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public static void test1() 
+    {        
+        Console.WriteLine("Basic Tests code");     
+        testing(CodingWithSquaredStringsTask.Code(data1), data1Sol); 
+        testing(CodingWithSquaredStringsTask.Code(data2), data2Sol); 
+        testing(CodingWithSquaredStringsTask.Code(""), ""); 
+        testing(CodingWithSquaredStringsTask.Decode(""), "");       
+        testing(CodingWithSquaredStringsTask.Decode(data2Sol), data2);        
+    }
+}
