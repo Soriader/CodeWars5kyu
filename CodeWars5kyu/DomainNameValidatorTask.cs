0d74570589f58e4a29ed274845e1ee34642006d5
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-04 12:51:32 +0200

    [~] Task: Domain name validator

---
+++ CodeWars5kyu/DomainNameValidatorTask.cs
@@ -0,0 +1,67 @@
+﻿namespace CodeWars5kyu;
+
+public class DomainNameValidatorTask
+{
+    public static bool Validate(string domain) 
+    {
+        if (string.IsNullOrEmpty(domain))
+        {
+            return false;
+        }        
+        if (domain.Length > 253)
+        {
+            return false;
+        }        
+        if (domain.Contains("@") || domain.Contains("..") || domain.Contains(" "))
+        {
+            return false;
+        }        
+        if (domain.StartsWith(".") || domain.EndsWith(".") || domain.StartsWith("-") || domain.EndsWith("-"))
+        {
+            return false;
+        }
+
+        var levels = domain.Split('.');
+        
+        if (levels.Length < 2)
+        {
+            return false;
+        }        
+        if (levels.Length > 127)
+        {
+            return false;
+        }
+        foreach (var level in levels)
+        {
+            if (level.Length == 0 || level.Length > 63)
+            {
+                return false;
+            }            
+            if (level.StartsWith("-") || level.EndsWith("-"))
+            {
+                return false;
+            }            
+            if (!level.All(c => char.IsLetterOrDigit(c) || c == '-'))
+            {
+                return false;
+            }
+            
+            if (!level.All(c => 
+                    (c >= 'a' && c <= 'z') || 
+                    (c >= 'A' && c <= 'Z') || 
+                    (c >= '0' && c <= '9') || 
+                    c == '-'))
+            {
+                return false;
+            }
+        }
+
+        var tld = levels.Last();
+        
+        if (tld.All(char.IsDigit))
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ UdemyTest/DomainNameValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace UdemyTest;
+
+using CodeWars5kyu;
+
+public class DomainNameValidator
+{
+    [Test]
+    public void DomainTests()
+    {
+        Assert.That(DomainNameValidatorTask.Validate("codewars"), Is.False);
+        Assert.That(DomainNameValidatorTask.Validate("g.co"), Is.True);
+        Assert.That(DomainNameValidatorTask.Validate("codewars.com"), Is.True);
+        Assert.That(DomainNameValidatorTask.Validate("CODEWARS.COM"), Is.True);
+        Assert.That(DomainNameValidatorTask.Validate("sub.codewars.com"), Is.True);
+        Assert.That(DomainNameValidatorTask.Validate("codewars.com-"), Is.False);
+        Assert.That(DomainNameValidatorTask.Validate(".codewars.com"), Is.False);
+        Assert.That(DomainNameValidatorTask.Validate("example@codewars.com"), Is.False);
+        Assert.That(DomainNameValidatorTask.Validate("127.0.0.1"), Is.False);
+    }
+}
